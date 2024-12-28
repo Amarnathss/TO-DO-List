09273bbc19fbdfe9d41e76984cfb6f65a98ed145
Amarnath S S <amarnathss306@gmail.com>
2024-12-28 18:38:31 +0530

    update 1

---
+++ src/context/TodoContext.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import {useContext , createContext} from 'react'
+
+export const TodoContext = createContext({
+    todos : [
+        { 
+            id:1,
+            todo: "todo msg",
+            comleted: false
+        }
+    ],
+    addTodo : (todo) => {},
+    updatedTodo : (id , todo)=>{},
+    deleteTodo : (id)=>{},
+    toggleComplete : (id)=>{}
+
+})
+
+export const useTodo = ()=>{
+    return useContext(TodoContext)
+
+}
+
+export const TodoProvider = TodoContext.Provider
